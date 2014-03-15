6223572ce3d1a52d0b8faf96c69a43f1fe9f3662
cebindani <danielecebin@hotmail.com>
2014-03-14 21:59:52 -0300

    just commit

---
+++ workspace/helloworld/src/br/com/dextra/helloworld/Hello.java
@@ -0,0 +1,16 @@
+package br.com.dextra.helloworld;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		saudacao("Daniele");
+		
+
+	}
+
+	System.out.println("Oi "+nome +"!");
+	static void saudacao(String nome){
+	}
+	
+}
---
+++ workspace/helloworld/src/br/com/dextra/helloworld/Ponto.java
@@ -0,0 +1,37 @@
+package br.com.dextra.helloworld;
+
+public class Ponto {
+
+	int x;
+	int y;
+
+	public Ponto(int a, int b) {
+		// TODO Auto-generated constructor stub
+		this.x = a;
+		this.y = b;
+	}
+
+	double distanciaAte(Ponto outro) {
+		return Math.sqrt(Math.pow(this.x - outro.x, 2)
+				+ Math.pow(this.y - outro.y, 2));
+		// o uso do this. é apenas para afirmar que está instanciando ele mesmo
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + "," + y + ")";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Ponto other = (Ponto) obj;
+		return this.x == other.x && this.y == other.y;
+	}
+
+	@Override
+	public int hashCode() {
+		return 3 * x + 7 * y;
+	}
+	
+	
+}
